a27ca3055397cf6835192d5b10d021e62ee00e61
Ana Tanaka <anapaulabtanaka@yahoo.com.br>
2024-08-21 23:18:04 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,12 +210,15 @@ themeButton.addEventListener("click", () => {
 });
 
 // Typing Animation using Typed JS
-var typed = new Typed(".type", {
-  strings: ["a Web", "an Android", "a Blockchain"],
-  smartBackspace: true,
-  startDelay: 1000,
-  typeSpeed: 130,
-  backDelay: 1000,
-  backSpeed: 60,
-  loop: true,
+// Wait until the DOM is fully loaded before initializing Typed.js
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".type", {
+    strings: ["a Web", "an Android", "a Blockchain"],
+    smartBackspace: true,
+    startDelay: 1000,
+    typeSpeed: 130,
+    backDelay: 1000,
+    backSpeed: 60,
+    loop: true,
+  });
 });
